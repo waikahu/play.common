d6f24d44ede51bd296feef104534efb7119138ce
Wagner Bolfe <shaper@live.com>
2023-02-23 10:56:40 -0300

    Added CosmosDB support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Common libraries used by Play Economy services.
 
 ## Create and publish package
 ```powershell
-$version="1.0.7"
+$version="1.0.8"
 $owner="waikahu"
 $gh_pat="[PAT HERE]"
 
--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -2,8 +2,15 @@ namespace Play.Common.Settings
 {
     public class MongoDbSettings
     {
+        private string connectionString;
+
         public string Host { get; init; }
         public int Port { get; init; }
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString
+        {
+            get { return string.IsNullOrWhiteSpace(connectionString) ? $"mongodb://{Host}:{Port}" : connectionString; }
+            init { connectionString = value; }
+        }
+        
     }
 }
