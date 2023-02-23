c99d2512ca86488449dc151b01cf4304e5161af4
Wagner Bolfe <shaper@live.com>
2023-02-23 15:22:21 -0300

    Added Azure Service Bus support

--- src/Play.Common/MassTransit/Extensions.cs
+++ src/Play.Common/MassTransit/Extensions.cs
@@ -13,6 +13,30 @@ namespace Play.Common.MassTransit
 {
     public static class Extensions
     {
+        private const string RabbitMq = "RABBITMQ";
+        private const string ServiceBus = "SERVICEBUS";
+
+        public static IServiceCollection AddMassTransitWithMessageBroker(
+            this IServiceCollection services,
+            IConfiguration configuration,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            switch (serviceSettings.MessageBroker?.ToUpper())
+            {
+                case ServiceBus:
+                    services.AddMassTransitWithServiceBus(configureRetries);
+                    break;
+
+                case RabbitMq:
+                default:
+                    services.AddMassTransitWithRabbitMq(configureRetries);
+                    break;
+            }
+            return services;
+        }
+
         public static IServiceCollection AddMassTransitWithRabbitMq(
             this IServiceCollection services,
             Action<IRetryConfigurator> configureRetries = null)
@@ -28,6 +52,41 @@ namespace Play.Common.MassTransit
             return services;
         }
 
+        public static IServiceCollection AddMassTransitWithServiceBus(
+            this IServiceCollection services,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+                configure.UsingPlayEconomyAzureServiceBus(configureRetries);
+            });
+
+            services.AddMassTransitHostedService();
+
+            return services;
+        }
+
+        public static void UsingPlayEconomyMessageBroker(
+            this IServiceCollectionBusConfigurator configure,
+            IConfiguration configuration,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            switch (serviceSettings.MessageBroker?.ToUpper())
+            {
+                case ServiceBus:
+                    configure.UsingPlayEconomyAzureServiceBus(configureRetries);
+                    break;
+
+                case RabbitMq:
+                default:
+                    configure.UsingPlayEconomyRabbitMq(configureRetries);
+                    break;
+            }
+        }
+
         public static void UsingPlayEconomyRabbitMq(
             this IServiceCollectionBusConfigurator configure,
             Action<IRetryConfigurator> configureRetries = null)
@@ -38,6 +97,27 @@ namespace Play.Common.MassTransit
                 var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
                 var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
                 configurator.Host(rabbitMQSettings.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+
+                if (configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
+        }
+
+        public static void UsingPlayEconomyAzureServiceBus(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator> configureRetries = null)
+        {
+            configure.UsingAzureServiceBus((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+                var serviceBusSettings = configuration.GetSection(nameof(ServiceBusSettings)).Get<ServiceBusSettings>();
+                configurator.Host(serviceBusSettings.ConnectionString);
                 configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
 
                 if (configureRetries == null)
